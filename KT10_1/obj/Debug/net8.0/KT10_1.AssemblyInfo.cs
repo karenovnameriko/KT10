ff87213682f6a3bdf8b43964ffe845eb35550a43
karenovnameriko <meri.terteryan.2007@mail.ru>
2025-11-18 23:43:03 +0300

    Add KT10

--- KT10_1/obj/Debug/net8.0/KT10_1.AssemblyInfo.cs
+++ KT10_1/obj/Debug/net8.0/KT10_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KT10_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3f3fe9d89acb1e0e9c4e7660732358248c4eec6")]
 [assembly: System.Reflection.AssemblyProductAttribute("KT10_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KT10_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
